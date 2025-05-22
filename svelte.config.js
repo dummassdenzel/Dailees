e03259cbba60a30e0008d8a6ec6f3ddccfcea525
dummassdenzel <denzelmanzperez1@gmail.com>
2025-05-23 03:55:24 +0800

    finish svelte setup

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html' // Required for routing to work in Capacitor
+		})
+	}
 };
 
 export default config;
